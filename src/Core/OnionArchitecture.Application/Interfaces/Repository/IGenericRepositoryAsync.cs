a999dbb96b655b9eabba2ca0db793baf6064cd0f
Osman Ulusan <osmanulusan34@hotmail.com>
2023-06-20 14:44:00 +0300

    Infrustructure içerisindeki generic repository yapısı oluşturuldu db context eklendi

--- src/Core/OnionArchitecture.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
+++ src/Core/OnionArchitecture.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
@@ -12,6 +12,6 @@ namespace OnionArchitecture.Application.Interfaces.Repository
     {
         Task<List<T>> GetAll();
         Task<T> GetById(Guid Id);
-        Task<T> AddAsync(T Entity);
+        Task<T> AddAsync(T entity);
     }
 }
