ef97bb40d172d3cef4fe2f65dc68c3eb84f933cb
Osman Ulusan <osmanulusan34@hotmail.com>
2023-06-20 14:14:28 +0300

    core tarafı ile ilgili bölümler tamamlandı

--- src/Core/OnionArchitecture.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
+++ src/Core/OnionArchitecture.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace OnionArchitecture.Application.Interfaces.Repository
 {
-    public interface IGenericRepostiroyAsync<T> where T : BaseEntity
+    public interface IGenericRepositoryAsync<T> where T : BaseEntity
     {
         Task<List<T>> GetAll();
         Task<T> GetById(Guid Id);
